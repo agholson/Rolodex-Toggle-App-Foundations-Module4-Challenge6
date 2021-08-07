5915a22a5429b01db8590e6276e1e5bc4fbe5c6d
agholson <87455899+agholson@users.noreply.github.com>
2021-08-07 17:24:14 -0400

    Initial Commit

---
+++ Challenge6CompaniesEnvironmentObject/Challenge6CompaniesEnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge6CompaniesEnvironmentObjectApp.swift
+//  Challenge6CompaniesEnvironmentObject
+//
+//  Created by Shepherd on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge6CompaniesEnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
